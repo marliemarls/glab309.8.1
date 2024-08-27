3914cb397f171cd57cfcc2da85a37d29dcd98afe
Marlie Martinez <marlieahue@gmail.com>
2024-08-27 14:42:01 -0400

    first commit

---
+++ src/main/java/com/perscholas/glab30981/RoleService.java
@@ -0,0 +1,9 @@
+package com.perscholas.glab30981;
+import java.util.List;
+
+public interface RoleService {
+    public void saveRole(Role role);
+    public Role findRoleByRoleName(String name);
+    public List<Role> getAllRoles();
+    public List<Role> getRolesByUser(long id);
+}
